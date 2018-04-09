14746e5bb32f1bff23ecc56daf6ad7a5b2443daa
Moises Vargas M <moisesweb@gmail.com>
2018-04-09 17:24:57 -0500

    KNN
    O(m*n)

--- retrieval/py/src/retrieval.py
+++ retrieval/py/src/retrieval.py
@@ -12,6 +12,32 @@ class Retrieval(object):
 
         return (nearest, nearest_distance)
 
+    def k_nearset_neighbor(self, x, docs, k=1, debug_steps=True):
+        if debug_steps:
+            steps = 1
+        nearests = [ (q, np.inf) for q in docs[:k] ]
+
+        for doc in docs:
+            if debug_steps:
+                steps += 1
+
+            doc_distance = self.distance(x, doc)
+
+            for i in range(k):
+                if debug_steps:
+                    steps += 1
+
+                i_distance = nearests[i][1]
+                if doc_distance < i_distance:
+                    if debug_steps:
+                        steps += 1
+                    nearests.insert(i, (doc, doc_distance))
+                    nearests = nearests[:k]
+                    break
+
+        print("Debugging steps", steps)
+        return nearests
+
     def distance(self, q1, q2):
         if None in [q1, q2]:
             return np.inf
--- retrieval/py/tests/test_retrieval.py
+++ retrieval/py/tests/test_retrieval.py
@@ -10,6 +10,15 @@ class TestPyRetrieval(object):
         docs = [1.0, 2.0, 3.1, 4.0]
         assert( 3.1 == r.one_nearest_neighbor(x, docs)[0] )
 
+    def test_k_nearest_neighbor(self):
+        r = Retrieval()
+        x = 9.0
+        docs = [5.0, 8.0, 9.2, 9.3, 9.9, 8.5, 13.0, 11.0, 8.9]
+        k3_neighbors = [x for (x, _) in r.k_nearset_neighbor(x, docs, k=3) ]
+        k6_neighbors = [x for (x, _) in r.k_nearset_neighbor(x, docs, k=6) ]
+        assert([8.9, 9.2, 9.3] == k3_neighbors)
+        assert([8.9, 9.2, 9.3, 8.5, 9.9, 8.0 ] == k6_neighbors)
+
     def test_distance(self):
         r = Retrieval()
         assert( r.distance(None, 3) == np.inf)
