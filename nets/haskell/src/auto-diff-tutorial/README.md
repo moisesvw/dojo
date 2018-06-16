654431be9bb7c1d3e414101228d8cb8f0496358e
Moises Vargas M <moisesweb@gmail.com>
2018-06-16 17:12:59 -0500

    setup cabal

--- nets/haskell/src/auto-diff-tutorial/README.md
+++ nets/haskell/src/auto-diff-tutorial/README.md
@@ -1,1 +1,9 @@
 # auto-diff-tutorial
+
+This project was initiated with Stack
+` stack new auto-diff-tutorial simple `
+
+Following Justin Le blog post https://blog.jle.im/entries/series/+functional-models.html
+
+to do
+install https://stackoverflow.com/questions/8961340/blas-and-lapack-libraries-required-for-compiling
--- nets/haskell/src/auto-diff-tutorial/src/Main.hs
+++ nets/haskell/src/auto-diff-tutorial/src/Main.hs
@@ -1,10 +1,27 @@
-module Main where
+import Numeric.Backprop
+import           Control.Monad.Trans.State
+import           Data.Bifunctor
+import           Data.Foldable
+import           Data.List
+import           Data.List.Split
+import           Data.Tuple
+import           Data.Type.Option
+import           GHC.Generics                          (Generic)
+import           GHC.TypeNats
+import           Lens.Micro hiding                     ((&))
+import           Numeric.Backprop
+import           Numeric.LinearAlgebra.Static.Backprop
+import           Numeric.LinearAlgebra.Static.Vector
+import           Numeric.OneLiner
+import           System.Random
+import qualified Data.Vector.Storable.Sized            as SVS
+import qualified Numeric.LinearAlgebra                 as HU
+import qualified Numeric.LinearAlgebra.Static          as H
+import qualified Prelude.Backprop                      as B
 
 type Model p a b = forall z. Reifies z W
   => Bvar z p
   -> Bvar z a
   -> Bvar z b
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+
